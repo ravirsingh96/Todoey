f9b04cf23b8c29dbab7a3e5486c56a6f7455e6e9
ravi singh <raviranjan@ravis-MacBook-Air.local>
2019-01-10 12:25:10 +0530

    Persisting data using codable protocol and plist.

---
+++ Todoey/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by ravi singh on 09/01/19.
+//  Copyright © 2019 ravi singh. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    
+    var title : String = ""
+    var done: Bool = false
+}
--- Todoey/ToDoViewController.swift
+++ Todoey/ToDoViewController.swift
@@ -10,12 +10,28 @@ import UIKit
 
 class ToDoViewController: UITableViewController {
     
-    let itemArray = ["buy apple","recharge  phone","buy drinks"]
+    var itemArray = [Item]()
+    
+ let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    print(dataFilePath)
+        
+      
+      loadItems()
+       // if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
+            
+       //     itemArray = items
+       // }
     }
+    
+    // Mark - Table View Method Delegate
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return itemArray.count
@@ -24,29 +40,88 @@ class ToDoViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+    
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ?.checkmark : .none
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        print (itemArray[indexPath.row])
+       itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        }else {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
-        }
-        
+       saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
+        
+    }
+    // Mark - Add new Item
+
+    @IBAction func barButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textfield = UITextField()
+        
+        let alert = UIAlertController.init(title: "Add New Todoey", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction.init(title: "Add Item", style: .default) { (UIAlertAction) in
+            print("sucess")
+            
+            let newItem = Item()
+            newItem.title = textfield.text!
+            
+       
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
+          
+        }
+        
+        
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add new Item"
+            textfield = alertTextField
+        }
+       
+        alert.addAction(action)
+        
+        present (alert , animated: true , completion: nil)
+    }
+    
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("encoding error item array \(error)")
+        }
+          tableView.reloadData()
+    }
+    
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            
+           do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("error decoding \(error)")
+            }
+            
+        }
         
     }
 
